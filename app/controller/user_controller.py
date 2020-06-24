b7fb27291854fe498a3a16c9745d634bcd46189c
Muhammad Rahman hasri <rahman.hasri@myboost.id>
2020-06-24 16:57:36 +0700

    add simple crud

--- app/controller/user_controller.py
+++ app/controller/user_controller.py
@@ -1,6 +1,6 @@
-from Flask import request
+from flask import request
 from app.model.user import Users
-from app import app
+from app import app, db
 from app.lib import response
 
 
@@ -36,14 +36,14 @@ def show(id):
     try:
         users = Users.query.filter_by(id=id).first()
         if not users:
-            return response.badRequest(null, 'Empty....')
+            return response.badRequest(None, 'Empty....')
 
         data = singleTransform(users)
         return response.ok(data, "")
     except Exception as e:
         print(e)
 
-def create():
+def store():
     try:
         name = request.json.get('name')
         email = request.json.get('email')
@@ -51,6 +51,45 @@ def create():
 
         new_user = Users(name=name, email=email)
         new_user.set_password(password)
-        db.session
+        db.session.add(new_user)
+        db.session.commit()
+
+        data = singleTransform(new_user)
+        return response.ok(data, 'Successfully create data!')
+    except Exception as e:
+        print(e)
+
+def update(id):
+    try:
+        name = request.json.get('name')
+        email = request.json.get('email')
+        password = request.json.get('password')
+        new_password = request.json.get('new_password')
+        user = Users.query.filter_by(id=id).first()
+        if not user:
+            return response.badRequest(None, 'Empty...')
+
+        # print('HERE YO')
+        if user.check_password(password) is False:
+            return response.badRequest(None, 'Wrong password')
+
+        user.name = name
+        user.email = email
+        user.set_password(new_password)
+        db.session.commit()
+        data = singleTransform(user)
+        return response.ok(data, "Successfully updated user with id: {}".format(id))
+    except Exception as e:
+        print(e)
+
+def delete(id):
+    try:
+        user = Users.query.filter_by(id=id).first()
+        if not user:
+            return response.badRequest(None, 'Empty...')
+        db.session.delete(user)
+        db.session.commit()
+
+        return response.ok({ "id": id }, 'Successfully delete data!')
     except Exception as e:
         print(e)
--- app/routes.py
+++ app/routes.py
@@ -12,14 +12,22 @@ def index():
     print(os.getenv('SECRET_KEY'))
     return "Hello Maman"
 
-@app.route('/users')
+@app.route('/users', methods=['GET'])
 def users():
     return user_controller.index()
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    return user_controller.store()
 
 @app.route('/users/<id>')
 def user_detail(id):
     return user_controller.show(id)
 
-@app.route('/users')
-def create_user():
-    return user_controller.create()
+@app.route('/users/<id>', methods=['PUT'])
+def update(id):
+    return user_controller.update(id)
+
+@app.route('/users/<id>', methods=['DELETE'])
+def delete(id):
+    return user_controller.delete(id)
